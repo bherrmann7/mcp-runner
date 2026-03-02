d3f5fcd280610ecf868326d82e599a444f79ad72
Bob Herrmann <bob@jadn.com>
2026-03-02 08:14:14 -0500

    Remove JIRA base URL from config.js
    Remove sensitive JIRA base URL from configuration.

--- config.js
+++ config.js
@@ -2,7 +2,6 @@
 export default {
   assigneeName: 'Bob Herrmann',
   projectKey: 'PS',
-  jiraBaseUrl: 'https://demoulas.atlassian.net',
   mcpCommand: '/Users/robertherrmann/bin/start-atlassian',
   storyPointsField: 'customfield_10026'
 };
